a13980bd48ea6c0366e3d982af62b15840d18b38
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2021-11-23 10:52:32 +0100

    created basic gallery with photo_manager

--- lib/provider/data_manager.dart
+++ lib/provider/data_manager.dart
@@ -1,10 +1,33 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class DataManager extends ChangeNotifier {
+  bool hasPermission = false;
   List<CameraDescription>? _cameras; // store the available cameras for later use
+
+  List<AssetPathEntity> albums = [];
+  AssetPathEntity? recentAlbum;
+  List<AssetEntity> images = [];
 
   List<CameraDescription>? get cameras => _cameras;
 
+  void init() async {
+    var result = await PhotoManager.requestPermissionExtend();
+    hasPermission = result.isAuth;
+  }
+
   setAvailableCameras(List<CameraDescription> cameras) => _cameras = cameras;
+
+  void getAlbums() async {
+    albums = await PhotoManager.getAssetPathList(type: RequestType.image, onlyAll: true);
+    recentAlbum = albums.first;
+  }
+
+  void getImages() async {
+    getAlbums();
+    final recentAssets = await recentAlbum!.getAssetListRange(start: 0, end: 100);
+    images = recentAssets;
+    notifyListeners();
+  }
 }
--- lib/views/gallery_screen.dart
+++ lib/views/gallery_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:magician_app/provider/data_manager.dart';
+import 'package:magician_app/widgets/asset_thumbnail.dart';
+import 'package:provider/provider.dart';
 
 class GalleryScreen extends StatelessWidget {
   const GalleryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Gallery'),
+    context.read<DataManager>().getImages();
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        // A grid view with 3 items per row
+        crossAxisCount: 3,
+      ),
+      itemCount: context.read<DataManager>().images.length,
+      itemBuilder: (_, index) {
+        return AssetThumbnail(asset: context.read<DataManager>().images[index]);
+      },
     );
   }
 }
