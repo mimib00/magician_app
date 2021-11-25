3dc99be4836a81d9e6f4ad330d042a7814045084
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2021-11-25 15:57:07 +0100

    added blur effect to camera background

--- lib/views/camera_screen.dart
+++ lib/views/camera_screen.dart
@@ -8,6 +8,7 @@ import 'package:magician_app/provider/data_manager.dart';
 import 'package:magician_app/utils/constants.dart';
 import 'package:magician_app/utils/magician_icons_icons.dart';
 import 'package:provider/provider.dart';
+import 'package:blur/blur.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({Key? key}) : super(key: key);
@@ -49,81 +50,99 @@ class _CameraScreenState extends State<CameraScreen> {
                 child: Align(
                   heightFactor: scale,
                   alignment: Alignment.bottomCenter,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    margin: const EdgeInsets.symmetric(vertical: 20),
-                    color: const Color(0xff181B30).withOpacity(.5),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            if (isFlashOn) {
-                              _controller.setFlashMode(FlashMode.off);
-                              setState(() {
-                                isFlashOn = false;
-                              });
-                            } else {
-                              _controller.setFlashMode(FlashMode.always);
-                              setState(() {
-                                isFlashOn = true;
-                              });
-                            }
-                          },
-                          icon: Icon(
-                            isFlashOn ? Icons.flash_on_rounded : Icons.flash_off_rounded,
-                            color: Colors.grey[200],
-                            size: 35,
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Blur(
+                          blur: 2.5,
+                          blurColor: const Color(0xff181B30).withOpacity(.5),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * .08,
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                            margin: const EdgeInsets.symmetric(vertical: 20),
                           ),
                         ),
-                        const SizedBox(width: 25),
-                        GestureDetector(
-                          onTap: () async {
-                            final image = await _controller.takePicture();
-
-                            final path = image.path;
-
-                            await AddToGallery.addToGallery(
-                              originalFile: File(path),
-                              albumName: 'Camera Demo',
-                              deleteOriginalFile: true,
-                            );
-                          },
-                          child: const CircleAvatar(
-                            backgroundColor: mainGrayColor,
-                            radius: 40,
-                            child: Center(
-                              child: Icon(
-                                MagicianIcons.camera,
-                                color: Colors.black,
-                                size: 30,
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          margin: const EdgeInsets.symmetric(vertical: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  if (isFlashOn) {
+                                    _controller.setFlashMode(FlashMode.off);
+                                    setState(() {
+                                      isFlashOn = false;
+                                    });
+                                  } else {
+                                    _controller.setFlashMode(FlashMode.always);
+                                    setState(() {
+                                      isFlashOn = true;
+                                    });
+                                  }
+                                },
+                                icon: Icon(
+                                  isFlashOn ? Icons.flash_on_rounded : Icons.flash_off_rounded,
+                                  color: Colors.grey[200],
+                                  size: 35,
+                                ),
                               ),
-                            ),
+                              const SizedBox(width: 25),
+                              GestureDetector(
+                                onTap: () async {
+                                  final image = await _controller.takePicture();
+
+                                  final path = image.path;
+
+                                  await AddToGallery.addToGallery(
+                                    originalFile: File(path),
+                                    albumName: 'Camera Demo',
+                                    deleteOriginalFile: true,
+                                  );
+                                },
+                                child: const CircleAvatar(
+                                  backgroundColor: mainGrayColor,
+                                  radius: 35,
+                                  child: Center(
+                                    child: Icon(
+                                      MagicianIcons.camera,
+                                      color: Colors.black,
+                                      size: 25,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 25),
+                              IconButton(
+                                onPressed: () {
+                                  if (context.read<DataManager>().cameras.length > 1) {
+                                    setState(() {
+                                      selectedCamera = selectedCamera == 0 ? 1 : 0;
+                                      context.read<DataManager>().initializeCamera(selectedCamera, context);
+                                    });
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                      content: Text('No secondary camera found'),
+                                      duration: Duration(seconds: 2),
+                                    ));
+                                  }
+                                },
+                                icon: Icon(
+                                  MagicianIcons.rotatecamera,
+                                  color: Colors.grey[200],
+                                  size: 30,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        const SizedBox(width: 25),
-                        IconButton(
-                          onPressed: () {
-                            if (context.read<DataManager>().cameras.length > 1) {
-                              setState(() {
-                                selectedCamera = selectedCamera == 0 ? 1 : 0;
-                                context.read<DataManager>().initializeCamera(selectedCamera, context);
-                              });
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                                content: Text('No secondary camera found'),
-                                duration: Duration(seconds: 2),
-                              ));
-                            }
-                          },
-                          icon: Icon(
-                            MagicianIcons.rotatecamera,
-                            color: Colors.grey[200],
-                            size: 30,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
