0fe9d23063358fdbd6f08ff6bff706a89173c05a
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2021-11-29 14:31:11 +0100

    added custom drop down

---
+++ lib/widgets/custom_drop_down.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:magician_app/utils/constants.dart';
+
+class CustomDropDown extends StatefulWidget {
+  final List<DropdownMenuItem<int>> items;
+  final int defaultSelectedItem;
+  const CustomDropDown({Key? key, required this.items, this.defaultSelectedItem = 0}) : super(key: key);
+
+  @override
+  State<CustomDropDown> createState() => _CustomDropDownState();
+}
+
+class _CustomDropDownState extends State<CustomDropDown> {
+  int selectedItem = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(11),
+        border: Border.all(
+          color: const Color(0xff8390C1),
+          width: 1,
+        ),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<int>(
+          value: widget.items[selectedItem].value,
+          onChanged: (value) {
+            setState(() {
+              selectedItem = value!;
+            });
+          },
+          isExpanded: true,
+          iconSize: 80,
+          iconEnabledColor: primaryColor,
+          dropdownColor: backgroundColor.withRed(50),
+          elevation: 1,
+          borderRadius: BorderRadius.circular(11),
+          items: widget.items,
+          icon: const Icon(
+            Icons.arrow_drop_down_rounded,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/* @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 90,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(11),
+        border: Border.all(
+          color: const Color(0xff8390C1),
+          width: 1,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: finalItems(),
+      ),
+    );
+  }*/
